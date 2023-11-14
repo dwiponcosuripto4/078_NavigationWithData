840b7e8efff75c832c4f41929eb3b2f3ecbafeb4
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-11-15 02:38:55 +0700

    create data pelanggan isnotempty

--- app/src/main/java/com/example/esjumbo/HalamanSatu.kt
+++ app/src/main/java/com/example/esjumbo/HalamanSatu.kt
@@ -2,11 +2,15 @@ package com.example.esjumbo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,7 +21,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 
@@ -40,50 +46,59 @@ fun HalamanSatu(
 
     var listDataTxt: MutableList<String> = mutableListOf(namaTxt, alamatTxt, telponTxt)
 
-    Column (
+    Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .padding(16.dp)
             .fillMaxSize()
-    ){
+    ) {
         OutlinedTextField(value = namaTxt, onValueChange = {
             namaTxt = it
         }, label = {
             Text(text = "Nama")
         })
+        Spacer(modifier = Modifier.padding(16.dp))
         OutlinedTextField(value = alamatTxt, onValueChange = {
             alamatTxt = it
         }, label = {
             Text(text = "Alamat")
         })
+        Spacer(modifier = Modifier.padding(16.dp))
         OutlinedTextField(value = telponTxt, onValueChange = {
             telponTxt = it
         }, label = {
             Text(text = "Telepon")
-        })
+        }, keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Phone
+        )
+            )
         Spacer(modifier = Modifier.padding(16.dp))
-        Button(onClick = { onSubmitButtonClick(listDataTxt) }) {
-            Text(text = stringResource(id = R.string.btn_submit))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .weight(1f, false),
+            horizontalArrangement =
+            Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            OutlinedButton(
+                modifier = Modifier.weight(1f),
+                onClick =
+                onCancelButtonClicked) {
+                Text(stringResource(R.string.cancel))
+            }
+            Button(
+                modifier = Modifier.weight(1f),
+                enabled = namaTxt.isNotEmpty() && alamatTxt.isNotEmpty() && telponTxt.isNotEmpty(),
+                onClick = { onSubmitButtonClick(listDataTxt) }
+            ) {
+                Text(stringResource(R.string.next))
+            }
         }
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
